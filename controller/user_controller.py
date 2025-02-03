40e95d03f3d93d2f54f4e87a03de6cdf4f7f3d12
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-02-03 11:48:29 +0545

    Control Model Architecture created

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
+from model.user_model import User_Model
+
+obj = User_Model()
 
 user_bp = Blueprint('user_bp', __name__)
 
 @user_bp.route('/user/signup')
 def signup():
-    return "This is SignUp Page"
+    return obj.user_signup_model()
---
+++ model/user_model.py
@@ -0,0 +1,3 @@
+class User_Model():
+    def user_signup_model(self):
+        return "this is user signup model"
