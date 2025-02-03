e6af730d185581a436c86c738f946ee31a2c731a
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-02-03 11:25:30 +0545

    created multiple files

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from user_controller import user_bp
 
 app = Flask(__name__)
+app.register_blueprint(user_bp)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return "This is Home Page"
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
