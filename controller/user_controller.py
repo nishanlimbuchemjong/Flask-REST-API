160b37139166e83aecb513e4fc2b0ef7fca22899
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2025-02-03 14:54:32 +0545

    POST method implemented using postgress

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,11 +1,16 @@
 from flask import Blueprint
 from model.user_model import User_Model
-
+from flask import request
 obj = User_Model()
 
 user_bp = Blueprint('user_bp', __name__)
 
 @user_bp.route('/user/getall')
 def user_get_all():
-    print("Output: ", obj)
     return obj.user_get_all_model()
+
+@user_bp.route('/user/add', methods=['POST'])
+def add_users():
+    print(request.form)
+    return obj.user_add_model(request.form)
+
--- model/user_model.py
+++ model/user_model.py
@@ -6,6 +6,7 @@ class User_Model():
         # connection establish:
         try:
             self.con = mysql.connector.connect(host='localhost', user='root', password='testing', database='flask_rest_api')
+            self.con.autocommit = True
             self.cur = self.con.cursor(dictionary=True)
             print("Connection established successfully")
         except:
@@ -19,3 +20,8 @@ class User_Model():
             return json.dumps(result)
         else:
             return "No Data Found"
+    
+    def user_add_model(self, data):
+        self.cur.execute(f"INSERT INTO users(name, email,  role, password, phone) VALUES('{data['name']}', '{data['email']}', '{data['role']}', '{data['password']}', '{data['phone']}')")
+       
+        return "User created successfully."
